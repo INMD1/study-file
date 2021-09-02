20a1d37db2df886f37c8d10ead72e58cc414ee63
INMD1 <78072355+INMD1@users.noreply.github.com>
2021-09-02 10:15:47 +0900

    수정

--- study/python/test.py
+++ study/python/test.py
@@ -1,12 +1,5 @@
 a = int(input(''))
 while(a):
     b = input('')
-    if b.count('char') >= 2:
-        if b.replace('char','') == '':
-            print('char')
-        else:
-            print(b.replace('char',''))
-    else:
-        print(b)
-    a = a - 1
+    .break
 
