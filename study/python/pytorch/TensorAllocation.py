b3226c66ed016000ffe53c95aee312df10c76cfb
INMD1 <lyw514549@gmail.com>
2021-09-30 23:50:48 +0900

    update

--- study/python/pytorch/TensorAllocation.py
+++ study/python/pytorch/TensorAllocation.py
@@ -1,5 +1,28 @@
 from typing import Match
 import torch
 
-t = torch.FloatStorage([0., 1., 2., 3., 4., 5., 6.])
+# 1차원 배열
+ts = torch.FloatTensor([0., 1., 2., 3., 4., 5., 6.])
+print(ts)
+
+print(ts.dim())
+print(ts.shape)
+print(ts.size())
+
+print(ts[0], ts[1], ts[-1]) 
+print(ts[2:5], ts[4:-1]) 
+print(ts[:2], ts[3:])    
+
+#2차원 배열
+t = torch.FloatTensor([[1., 2., 3.],
+                       [4., 5., 6.],
+                       [7., 8., 9.],
+                       [10., 11., 12.]
+                      ])
 print(t)
+
+print(t.dim())
+print(t.shape)
+print(t.size())
+
+print(t[:,:-1])
---
+++ study/python/study/for.py
@@ -0,0 +1,6 @@
+a = 0
+b = 0
+
+for 0 in 5:
+    a = a + b
+    print(a)
