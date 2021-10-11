e5f44e7200af6871e67470638e50341e735a96a1
INMD1 <lyw514549@gmail.com>
2021-10-12 01:42:51 +0900

    업데이트

---
+++ study/Kotlin/src/loopcmd.kt
@@ -0,0 +1,15 @@
+import java.io.IOException
+import java.util.*
+
+fun main(){
+    val sc = Scanner(System.`in`)
+//    try {
+//        print("숫자만 입력해라 휴면: ")
+//        var x = sc.nextInt()
+//        print("너가 친 숫자는 : " + x + "이구나\n")
+//    } catch (e: Exception) {
+//        print("뭐 잘못침?\n")
+//    }
+
+
+}
